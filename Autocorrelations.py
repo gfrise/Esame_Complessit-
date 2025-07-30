595eaf83ec1f36e3a2b2393693af38db2197f8b4
gfrise <104461249+gfrise@users.noreply.github.com>
2025-07-30 11:07:45 +0200

    deleted double

--- Autocorrelations.py
+++ Autocorrelations.py
@@ -1,4 +1,5 @@
-import numpy as np
+import numpy as np, matplotlib.pyplot as plt
+import random
 
 def time_avg(x,tau):
     n = len(x)-tau
@@ -8,6 +9,22 @@ def time_avg(x,tau):
     return acc/n
 
 def ensemble_avg
+
+t = 100
+dt = 1/t
+n = 100
+nn = t*n
+y = 0.1
+m = 50
+
+for _ in range(m):
+    x = np.empty(nn)
+    x[0] = 0.3
+    noise = np.random.normal(0,3,nn)
+    for i in range(1,nn):
+        x[i]=x[i-1]-y*dt*x[i-1]+np.sqrt(dt)*noise[i]
+        ac[i]+=x[0]*x[k]
+    
     
 
    
