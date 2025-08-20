c06610fda97d8be19d757721375018028a95fb9e
gfrise <104461249+gfrise@users.noreply.github.com>
2025-08-20 16:57:48 +0200

    refinements

--- Shuffling.py
+++ Shuffling.py
@@ -3,19 +3,19 @@ from numpy.random import randint
 
 x = np.random.uniform(1,101,10) #float
 n = len(x)
-#NAIVE introduce un bias statistico
+#NAIVE bias statistico
 for i in range(n):
-    j = randint(n) # tra 0 e n-1
+    j = randint(n) # [0,n-1]
     x[j], x[i] = x[i], x[j]
 
-#DUSTERNFELD, ottimizza fisher yates, permutazioni uniformi senza ripetizione, shuffla sull'array originale senza copie intermedie, più veloce, shuffla in avanti, fisher indietro
+#DUSTERNFELD è fisher ottimizzato e veloce, shuffla in avanti invece che indietro senza array intermedi, permutazioni uniformi no ripetizione 
 for i in range(n-1): # range(n-1,0,-1) e randint(0,i+1) per dusternfeld e fisher yates è quello accapo
-    j = randint(i,n) # j = i + randint(n-i) da 0 a n-i
+    j = randint(i,n) # j = i +randint(n-i) [0,n-i]
     x[j], x[i] = x[i], x[j]
 
-#SATTOLO j>1 no swap con se o precedenti, permutazioni cicliche senza ripetizioni, j da 0 a n-i-1
+#SATTOLO j>1 no swap con se o precedenti, permutazioni cicliche no ripetizioni
 for i in range(n-1):
-    j = randint(i+1,n)  # j = i+1 + randint(n-i-1)
+    j = randint(i+1,n)  # j = i+1 +randint(n-i-1) [0,n-i-1]
     x[j], x[i] = x[i], x[j]
 
 #BOOTSTRAP campiona con ripetizioni
